19d91c30a6b1d10fb3b121689a87e2e0cb689f7e
peter bull <17122363+peterbull@users.noreply.github.com>
2026-07-11 16:00:12 -0400

    2d ops

--- cuda/hello.cu
+++ cuda/hello.cu
@@ -6,15 +6,20 @@ __global__ void helloKernel() {
 }
 
 // general structure for a kernel:
-// -> grid (1 per kernel)
+// -> grid (1 per kernel, many dimensions)
 //   -> blocks (65535 per dimension)
 //   -> threads (1024 per block)
 //   -> streaming multiprocessors(SM) (48)
 //   -> warp size (32 threads per SM)
-__device__ void printThreadIdx() { printf("thread: %d\n", threadIdx.x); }
-__device__ void printBlockIdx() { printf("blockIdx: %d\n", blockIdx.x); }
-__device__ void printBlockDim() { printf("blockDim: %d\n", blockDim.x); }
 
+__device__ void printKernelInfo() {
+  printf("thread idx x: %d\n", threadIdx.x);
+  printf("thread idx y: %d\n", threadIdx.y);
+  printf("block idx x: %d\n", blockIdx.x);
+  printf("block idx y: %d\n", blockIdx.y);
+  printf("block dim x: %d\n", blockDim.x);
+  printf("block dim y: %d\n", blockDim.y);
+}
 void printFloatArray(float *arr, int n) {
   for (int i = 0; i < n; i++) {
     printf("array element %d: %f\n", i, arr[i]);
@@ -24,7 +29,6 @@ void printFloatArray(float *arr, int n) {
 // implement a kernel that adds 10 to each position of vector
 // `a` and stores it in vector `out`. 1 thread per position.
 __global__ void addTen(float *a, float *out) {
-  printThreadIdx();
   out[threadIdx.x] = a[threadIdx.x] + 10;
 }
 void runAddTen() {
@@ -44,7 +48,6 @@ void runAddTen() {
 // a kernel that adds together each position of `a` and `b`
 // and stores it in `out`. 1 thread per position.
 __global__ void combineAB(float *a, float *b, float *out) {
-  printThreadIdx();
   out[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x];
 };
 
@@ -66,10 +69,10 @@ void runCombineAB() {
   printFloatArray(out, len);
 }
 
+// guarding
 // implement a kernel that adds 10 to each position of `a` and stores it in
 // `out`. more threads than positions.
 __global__ void addTenManyThreads(float *a, float *out, int length) {
-  printThreadIdx();
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < length) {
     printf("adding in flat index:%d, thread index: %d\n", idx, threadIdx.x);
@@ -91,6 +94,46 @@ void runAddTenManyThreads() {
   printFloatArray(out, 8);
 }
 
+// 2d map
+// implement a kernel that adds 10 to each position of `a` and stores it in
+// `out`. Input `a` is 2D and square. more threads than positions.
+__global__ void addTen2dSquare(float *a, float *out, int width, int height) {
+  printKernelInfo();
+  // (4,3)
+  //
+  // [1,2,3,4]
+  // [5,6,7,8]
+  // [9,10,11,12]
+  int row = threadIdx.y;
+  int col = threadIdx.x;
+  if (col < width && row < height) { // guard each axis before flattening
+    int i = row * width + col;
+    out[i] = a[i] + 10;
+  }
+}
+
+void runAddTen2dSquare() {
+  float *d_a;
+  float *d_out;
+  int width = 4;
+  int height = 3;
+  int n = width * height;
+
+  float a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  float out[12];
+
+  cudaMalloc(&d_a, n * sizeof(float));
+  cudaMalloc(&d_out, n * sizeof(float));
+  cudaMemcpy(d_a, a, n * sizeof(float), cudaMemcpyHostToDevice);
+  dim3 blockDim(
+      4, 4); // set to slightly bigger than the array to practice guarding
+  dim3 gridDim(1, 1);
+  addTen2dSquare<<<gridDim, blockDim>>>(d_a, d_out, width, height);
+  cudaMemcpy(out, d_out, n * sizeof(float), cudaMemcpyDeviceToHost);
+
+  printFloatArray(out, n);
+}
+
 int main() {
   // float *d_a;
   // float *d_out;
@@ -108,7 +151,7 @@ int main() {
   // helloKernel<<<1028, 4>>>();
   // runAddTen();
   // runCombineAB();
-  runAddTenManyThreads();
+  runAddTen2dSquare();
   cudaDeviceSynchronize();
   return 0;
 }
